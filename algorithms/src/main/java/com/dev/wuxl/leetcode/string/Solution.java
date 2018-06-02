fdebcfcb4ae376d834db3165c965528f59f36ff9
wu.xuanle <wu.xuanle@immomo.com>
2018-06-02 14:12:25 +0800

    fix

---
+++ algorithms/src/main/java/com/dev/wuxl/leetcode/string/Solution.java
@@ -0,0 +1,8 @@
+package com.dev.wuxl.leetcode.string;
+
+/**
+ * @author <a href="mailto:wu.xuanle@immomo.com">wu.xuanle</a>
+ * @create 18/6/2
+ */
+public class Solution {
+}
---
+++ algorithms/src/main/java/com/dev/wuxl/others/Solution.java
@@ -0,0 +1,13 @@
+package com.dev.wuxl.others;
+
+/**
+ * @author <a href="mailto:wu.xuanle@immomo.com">wu.xuanle</a>
+ * @create 18/6/2
+ */
+public class Solution {
+
+
+
+
+
+}
